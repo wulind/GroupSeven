9e93445d3135c74c349591da8e10929faa7bd7fb
wulind <31117435+wulind@users.noreply.github.com>
2018-11-09 13:34:23 -0500

    Update EventManager.h

--- include/EventManager.h
+++ include/EventManager.h
@@ -7,29 +7,29 @@
 
 namespace escape{
   class EventManager{
-  public:
-      //Constructor
-      EventManager(sf::RenderWindow *App, Platform *platform, GameState *state);
-      //Handles clicking the mouse on the finish button
-      void clickButton(GameState *state, sf::Sprite finishButton);
-      //These methods handle platform moving
-      void updateMouse();
-      bool checkMouseOverPlatform(Platform *platform);
-      void releaseAllPlatforms(Platform *platform);
-	
-  private:
-      //The RenderWindow
-      sf::RenderWindow *App;
-      //The current game state
-      GameState *state;
-      //The platform object
-      Platform *platform;
-      //The relative positions are used with window resizing.
-      int relativeX;
-      int relativeY;
-      //The size of the sprite
-      sf::FloatRect size;
+	  public:
+	      //Constructor
+	      EventManager(sf::RenderWindow *App, Platform *platform, GameState *state);
+	      //Handles clicking the mouse on the finish button
+	      void clickButton(GameState *state, sf::Sprite finishButton);
+	      //These methods handle platform moving
+	      void updateMouse();
+	      bool checkMouseOverPlatform(Platform *platform);
+	      void releaseAllPlatforms(Platform *platform);
+
+	  private:
+	      //The RenderWindow
+	      sf::RenderWindow *App;
+	      //The current game state
+	      GameState *state;
+	      //The platform object
+	      Platform *platform;
+	      //The relative positions are used with window resizing.
+	      int relativeX;
+	      int relativeY;
+	      //The size of the sprite
+	      sf::FloatRect size;
     };
-    }
+}
 
 #endif
