17c9dc0dfdcd25ea3d28d1494cfd168ddaa086c0
Linda Y  Wu <lywu01@tweety.cs.wm.edu>
2018-11-27 19:09:23 -0500

    found workaround for seg fault where transformation would not set on platform body

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -71,16 +71,16 @@ void Platform::updateDragPosition(float mouseX, float mouseY) {
 	else{
 		this -> xCoord = mouseX - mouseDragOffsetX;
 		this -> yCoord = mouseY - mouseDragOffsetY;
-		this -> Body -> SetTransform(b2Vec2(this -> xCoord / SCALE, this -> yCoord / SCALE), M_PI);
+		this -> Body -> SetTransform(b2Vec2(this -> xCoord / SCALE, this -> yCoord / SCALE), this -> rotation * b2_pi / 180);
 	}
 }
 
 /*
 * Set rotational value for a platform
+* @param rotation: rotation of object in degrees
 */
 void Platform::setRotation(int rotation){
 	this -> rotation = rotation;
-	//this -> Body -> SetTransform(b2Vec2(this -> xCoord / SCALE, this -> yCoord / SCALE), this -> rotation * b2_pi / 180 );
-
+	// this -> Body -> SetTransform(b2Vec2(this -> xCoord / SCALE, this -> yCoord / SCALE), rotation * b2_pi / 180 );
 
 }
