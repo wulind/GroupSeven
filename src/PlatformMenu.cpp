c3097c99a2ebeb882a3d0715149d26c997cf3ec2
Linda Y  Wu <lywu01@astro.cs.wm.edu>
2018-12-01 16:38:02 -0500

    deleting line that's not needed

--- src/PlatformMenu.cpp
+++ src/PlatformMenu.cpp
@@ -8,5 +8,4 @@ PlatformMenu::PlatformMenu(){
   this -> title.setPosition(635, 5);
   this -> title.setString("Click & Drag");
   this -> title.setCharacterSize(24);
-  this -> title.setFillColor(sf::Color::White);
 }
