95820a93c56c9e2bb04a7b8a8b4d6eb6f00d67e3
wulind <31117435+wulind@users.noreply.github.com>
2018-12-16 20:36:11 -0500

    fixing spacing and updating documentation

--- src/Goal.cpp
+++ src/Goal.cpp
@@ -8,22 +8,24 @@ using namespace escape;
 * Constructor
 */
 Goal::Goal(){
-  this -> width = 250;
-  this -> height = 110;
+	this -> width = 250;
+	this -> height = 110;
 
-  this -> xCoord = 350;
-  this -> yCoord = 550;
+	this -> xCoord = 350;
+	this -> yCoord = 550;
 }
 
 /*
 * Constructor
+* @param xCoord: x coordinate of the goal
+* @param yCoord: y coordinate of the goal
 */
 Goal::Goal(int xCoord, int yCoord){
-  this -> width = 250;
-  this -> height = 110;
+	this -> width = 250;
+	this -> height = 110;
 
-  this -> xCoord = xCoord;
-  this -> yCoord = yCoord;
+	this -> xCoord = xCoord;
+	this -> yCoord = yCoord;
 }
 
 /*
@@ -33,53 +35,54 @@ Goal::Goal(int xCoord, int yCoord){
 void Goal::setWorld(b2World* World){
 	//Creates Box2D Body. Static body so that It won't move.
 	b2BodyDef BodyDef;
-  BodyDef.position = b2Vec2(this -> xCoord/SCALE, this -> yCoord/SCALE);
-  BodyDef.type = b2_staticBody;
-  this -> Body = World -> CreateBody(&BodyDef);
+	BodyDef.position = b2Vec2(this -> xCoord/SCALE, this -> yCoord/SCALE);
+	BodyDef.type = b2_staticBody;
+	this -> Body = World -> CreateBody(&BodyDef);
 
-  b2PolygonShape shape;
-  const float density = 100.f;
+	b2PolygonShape shape;
+	const float density = 100.f;
 
-  shape.SetAsBox(2/SCALE, this -> height/4/SCALE, b2Vec2((-this -> width/2 + 4)/SCALE, this -> height/4/SCALE), 0);//Headlights of van
-  this -> Body -> CreateFixture(&shape, density);
+	shape.SetAsBox(2/SCALE, this -> height/4/SCALE, b2Vec2((-this -> width/2 + 4)/SCALE, this -> height/4/SCALE), 0);//Headlights of van
+	this -> Body -> CreateFixture(&shape, density);
 
-  shape.SetAsBox(10/SCALE, this -> height/SCALE, b2Vec2((-this -> width/2 + 55)/SCALE, this -> height/2/SCALE), 0);//Left van body
-  this -> Body -> CreateFixture(&shape, density);
+	shape.SetAsBox(10/SCALE, this -> height/SCALE, b2Vec2((-this -> width/2 + 55)/SCALE, this -> height/2/SCALE), 0);//Left van body
+	this -> Body -> CreateFixture(&shape, density);
 
-  shape.SetAsBox(this -> width - 10/SCALE, 5/SCALE, b2Vec2((-this -> width/2 + 10)/SCALE, (this -> height/2 - 15)/SCALE), 0);//Bottom van body
-  this -> Body -> CreateFixture(&shape, density);
+	shape.SetAsBox(this -> width - 10/SCALE, 5/SCALE, b2Vec2((-this -> width/2 + 10)/SCALE, (this -> height/2 - 15)/SCALE), 0);//Bottom van body
+	this -> Body -> CreateFixture(&shape, density);
 
-  shape.SetAsBox(5/SCALE, this -> height/SCALE, b2Vec2((this -> width/2 - 20)/SCALE, this -> height/2/SCALE), 0);//Right van body & butt of the van
-  this -> Body -> CreateFixture(&shape, density);
+	shape.SetAsBox(5/SCALE, this -> height/SCALE, b2Vec2((this -> width/2 - 20)/SCALE, this -> height/2/SCALE), 0);//Right van body & butt of the van
+	this -> Body -> CreateFixture(&shape, density);
 }
 
 /*
 * Detects win
-* @param &stolenObject: stolen object needed to detect collision
+* @param stolenObject: stolen object needed to detect collision
 */
 int Goal::detectWin(StolenObject &stolenObject){
-  //Checks if object is awake, and if it is moving slow enough, then kill it if its moving too slowly
-  if(stolenObject.Body -> IsAwake()){
-    if(stolenObject.Body -> GetLinearVelocity().y == 0 && abs(stolenObject.Body -> GetLinearVelocity().x) < 1 && !(stolenObject.Body -> GetLinearVelocity().y == 0 && stolenObject.Body -> GetLinearVelocity().x == 0)){
-      stolenObject.killSpeed();
-    }
-    if(stolenObject.yCoord + (stolenObject.radius * 2) < 0){//For underwater levels
-      stolenObject.killSpeed();
-    }
-    return -1;
-  }
+	//Checks if object is awake, and if it is moving slow enough, then kill it if its moving too slowly
+	if(stolenObject.Body -> IsAwake()){
+		if(stolenObject.Body -> GetLinearVelocity().y == 0 && abs(stolenObject.Body -> GetLinearVelocity().x) < 1 && !(stolenObject.Body -> GetLinearVelocity().y == 0 && stolenObject.Body -> GetLinearVelocity().x == 0)){
+		stolenObject.killSpeed();
+		}
+		
+		if(stolenObject.yCoord + (stolenObject.radius * 2) < 0){//For underwater levels
+		stolenObject.killSpeed();
+		}
+		return -1;
+	}
 
-  //Determines the left, right, and top boundaries
-  bool left = stolenObject.bounds.left > this -> bounds.left ? true: false;
-  bool right = stolenObject.bounds.left + stolenObject.bounds.width < this -> bounds.left + this -> width ? true: false;;
-  bool top = stolenObject.bounds.top > this -> bounds.top ? true: false;
+	//Determines the left, right, and top boundaries
+	bool left = stolenObject.bounds.left > this -> bounds.left ? true: false;
+	bool right = stolenObject.bounds.left + stolenObject.bounds.width < this -> bounds.left + this -> width ? true: false;;
+	bool top = stolenObject.bounds.top > this -> bounds.top ? true: false;
 
-  //Detects of ball is in goal and alive
-  if((left && right && top) || stolenObject.yCoord + (stolenObject.radius * 2) < 0){
-    if (stolenObject.health != 0){
-      return 1;
-    }
-  }
+	//Detects of ball is in goal and alive
+	if((left && right && top) || stolenObject.yCoord + (stolenObject.radius * 2) < 0){
+		if (stolenObject.health != 0){
+		return 1;
+		}
+	}
 
-  return 0;
+	return 0;
 }
